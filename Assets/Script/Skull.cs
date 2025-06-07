1d21417116e60c44cdc30029d1930e1872e64d3c
Defaulthyu <defaulthyu@gmail.com>
2025-06-07 14:27:07 +0900

    [Feat] 몬스터 애니메이션, 플레이어 추적, 몬스터 재배치 구현

---
+++ Assets/Script/Skull.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skull : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if(!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rb.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
+        rb.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+            return;
+
+        sr.flipX = target.position.x < rb.position.x;
+    }
+}
