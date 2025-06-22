4bca224139a1a9f2e1e6e3f5885099d04f4fad00
Defaulthyu <defaulthyu@gmail.com>
2025-06-22 09:22:06 +0900

    [Fix] 레벨마다 적 프리펩 여러개 나오도록 수정. 밸런스 수정

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -1,49 +1,74 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
-    public SpawnData[] spawnData;
+
+    public List<SpawnLevelData> levelSpawnDataList; // 이제 인스펙터에 보임!
+
     public float levelTime;
-
-
     int level;
+    int prevLevel = -1; // 이전 레벨을 저장하는 변수
+    List<float> spawnTimers = new List<float>();
 
-    float timer;
+
 
     void Start()
     {
         spawnPoints = GetComponentsInChildren<Transform>();
-        levelTime = GameManager.instance.maxGameTime / spawnData.Length;
+        levelTime = GameManager.instance.maxGameTime / levelSpawnDataList.Count;
+
+        foreach (var data in levelSpawnDataList[0].spawnDatas)
+            spawnTimers.Add(0f);
     }
+
     void Update()
     {
-        if(!GameManager.instance.isLive)
-        {
+        if (!GameManager.instance.isLive)
             return;
-        }
-        timer += Time.deltaTime;
-        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / levelTime), spawnData.Length - 1);
 
-        if (timer > spawnData[level].spawnTime)
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / levelTime), levelSpawnDataList.Count - 1);
+
+        if (level != prevLevel)
         {
-            timer = 0;
-            Spawn();
+            Debug.Log($"[Spawner] ▶ 레벨 {level} 진입 (게임 시간: {GameManager.instance.gameTime:F1}s)");
+            prevLevel = level;
         }
 
+        SpawnData[] currentLevelData = levelSpawnDataList[level].spawnDatas;
+
+        while (spawnTimers.Count < currentLevelData.Length)
+            spawnTimers.Add(0f);
+
+        for (int i = 0; i < currentLevelData.Length; i++)
+        {
+            spawnTimers[i] += Time.deltaTime;
+
+            if (spawnTimers[i] > currentLevelData[i].spawnTime)
+            {
+                spawnTimers[i] = 0f;
+                Spawn(currentLevelData[i]);
+            }
+        }
     }
 
-    void Spawn()
+    void Spawn(SpawnData data)
     {
-        GameObject enemy = GameManager.instance.pool.Get(spawnData[level].prefabId);
+        GameObject enemy = GameManager.instance.pool.Get(data.prefabId);
         enemy.transform.position = spawnPoints[Random.Range(1, spawnPoints.Length)].position;
-        enemy.GetComponent<Skull>().Init(spawnData[level]);
-        Debug.Log("SpawnData prefabId: " + spawnData[level].prefabId);
-
+        enemy.GetComponent<Skull>().Init(data);
     }
 }
+
+
+[System.Serializable]
+public class SpawnLevelData
+{
+    public SpawnData[] spawnDatas; // 이게 인스펙터에 보여질 배열!
+}
+
+
 
 [System.Serializable]
 public class SpawnData
