bedd341c02f30cec7fe3f4fb485fa04d8cf52350
Defaulthyu <defaulthyu@gmail.com>
2025-06-22 21:38:29 +0900

    [Feat] 골드 사용 상점 구현

---
+++ Assets/Script/UIClickManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIClickManager : MonoBehaviour
+{
+    public int goldCost = 100; // 레벨업에 필요한 골드 비용
+    public void OpenLevelUpByGold()
+    {
+
+        if (GoldManager.Instance.currentGold >= goldCost)
+        {
+            GoldManager.Instance.AddGold(-goldCost);
+            GameManager.instance.uiLevelUp.Show();
+        }
+        else
+        {
+            Debug.Log("골드가 부족합니다.");
+        }
+    }
+}
